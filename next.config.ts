6e8eb2db2bf80983ec811d5914493b28e4a79c3f
prakashPranit <pranitprakashdev@gmail.com>
2025-12-15 14:02:06 +0530

    feat: Add build-time validation for `DATABASE_URL` in production and modify its value in `.env`.

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,16 @@
 import type { NextConfig } from "next";
+import { type PrismaClient } from "@prisma/client";
+
+// Validate environment variables during build
+if (
+  process.env.NODE_ENV === "production" &&
+  !process.env.DATABASE_URL
+) {
+  throw new Error(
+    "DATABASE_URL is missing. Please add it to your environment variables."
+  );
+}
+
 
 const nextConfig: NextConfig = {
   output: "standalone",
